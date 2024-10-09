c2531e8f30eb9f2b44950385f348ab3ab7338a83
EddVargs8 <l19290961@tectijuana.edu.mx>
2024-10-09 15:18:20 -0700

    Postgresql database implementation using prisma and Neon.tech service

--- src/app/components/navbar_Layout.js
+++ src/app/components/navbar_Layout.js
@@ -3,7 +3,7 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const isAuthenticated = true;
+const isAuthenticated = false;
 
 const navigation = [
   { name: 'Inicio', href: '/', current: true },
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,17 @@
-
 import Navbar_Layout from './components/navbar_Layout';
+import { prisma } from './utils/prisma';
+
+async function getUserById(id) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id, 
+    },
+  })
+  return user
+}
+
+// Llamada a la función para obtener el usuario por id
+getUserById(1).then(user => console.log(user)).catch(err => console.error(err))
 
 export default function Home() {
   return (
