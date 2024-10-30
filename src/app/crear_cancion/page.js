1644a1f2d0d8adbcf212bb3980b051c55d670f31
EddVargs8 <l19290961@tectijuana.edu.mx>
2024-10-29 22:55:33 -0700

    DrumsRNN 1.0

--- src/app/crear_cancion/page.js
+++ src/app/crear_cancion/page.js
@@ -2,59 +2,52 @@
 "use client";
 
 import { useEffect } from 'react';
+import { seeds } from '../seeds';
 
 export default function Page() {
     useEffect(() => {
-        const MELODY = {
-            notes: [
-                { pitch: 64, quantizedStartStep: 0, quantizedEndStep: 1 },  // E
-                { pitch: 64, quantizedStartStep: 1, quantizedEndStep: 2 },  // E
-                { pitch: 65, quantizedStartStep: 2, quantizedEndStep: 3 },  // F
-                { pitch: 67, quantizedStartStep: 3, quantizedEndStep: 4 },  // G
-                { pitch: 67, quantizedStartStep: 4, quantizedEndStep: 5 },  // G
-                { pitch: 65, quantizedStartStep: 5, quantizedEndStep: 6 },  // F
-                { pitch: 64, quantizedStartStep: 6, quantizedEndStep: 7 },  // E
-                { pitch: 62, quantizedStartStep: 7, quantizedEndStep: 8 },  // D
-                { pitch: 60, quantizedStartStep: 8, quantizedEndStep: 9 },  // C
-                { pitch: 60, quantizedStartStep: 9, quantizedEndStep: 10 }, // C
-                { pitch: 62, quantizedStartStep: 10, quantizedEndStep: 11 }, // D
-                { pitch: 64, quantizedStartStep: 11, quantizedEndStep: 12 }, // E
-                { pitch: 64, quantizedStartStep: 12, quantizedEndStep: 13 }, // E
-                { pitch: 62, quantizedStartStep: 13, quantizedEndStep: 14 }, // D
-                { pitch: 62, quantizedStartStep: 14, quantizedEndStep: 15 }, // D
-            ],
-            quantizationInfo: { stepsPerQuarter: 4 },
-            tempos: [{ time: 0, qpm: 110 }],
-            totalQuantizedSteps: 15,
-        };
-
-        const config = {
-            noteHeight: 10,
-            pixelsPerTimeStep: 200,  // like a note width
-            noteSpacing: 2,
-            noteRGB: '194, 24, 7',
-            activeNoteRGB: '87, 35, 100',
-        };
-
-        let viz = new mm.PianoRollCanvasVisualizer(MELODY, document.getElementById('canvas'), config);
-
-        const music_rnn = new mm.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/basic_rnn');
-        music_rnn.initialize();
+        let viz = null; 
+        const drums_rnn = new mm.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/drum_kit_rnn');
+        drums_rnn.initialize();
+        
 
         const rnnPlayer = new mm.Player(false, {
-            run: (note) => {
-                console.log(note); 
+            run: (note) => { 
                 viz.redraw(note); // Actualiza la visualización de la nota activa
             },
             stop: () => {
-                console.log('Reproducción terminada');
-            },
+                console.log('Player detenido.');
+            }
         });
 
-        const button = document.getElementById('button');
+        const playBtn = document.getElementById('play');
+        const stopBtn = document.getElementById('stop');
 
-        const handleClick = () => {
+        function getRandomSeed(seedsArray) {
+            const randomIndex = Math.floor(Math.random() * seedsArray.length);
+            return seedsArray[randomIndex];
+        }
+
+        function setupVisualizer(sequence) {
+            const config = {
+                noteHeight: 10,
+                pixelsPerTimeStep: 200,  // like a note width
+                noteSpacing: 2,
+                noteRGB: '194, 24, 7',
+                activeNoteRGB: '87, 35, 100',
+            };
+    
+            viz = new mm.PianoRollCanvasVisualizer(sequence, document.getElementById('canvas'), config);
+        }
+
+        const playClick = () => {
             
+            const randomDrumSeed = getRandomSeed(seeds.drumsSeeds);
+            console.log('Secuencia aleatoria:', randomDrumSeed.id);
+            setupVisualizer(randomDrumSeed);
+
+
+
             if (mm.Player.tone.state === 'started') {
                 console.log('La secuencia ya está en reproducción. Deteniendo...');
                 rnnPlayer.stop().then(() => {
@@ -67,22 +60,23 @@ export default function Page() {
             // Iniciar AudioContext
             mm.Player.tone.start().then(() => {
                 console.log('AudioContext iniciado');
-                music_rnn.continueSequence(MELODY, 25, 3)
+                drums_rnn.continueSequence(randomDrumSeed, 24, 0.8)
                     .then((sample) => {
                         console.log('Iniciando nueva secuencia.');
 
-                        const offset = MELODY.totalQuantizedSteps;
+                        const offset = randomDrumSeed.totalQuantizedSteps;
                             sample.notes.forEach(note => {
                             note.quantizedStartStep += offset;
                             note.quantizedEndStep += offset;
                         });
 
+                        // Combina `randomDrumSeed` con `sample` en `combinedSequence`
                         const combinedSequence = {
-                            ...MELODY,
-                            notes: [...MELODY.notes, ...sample.notes], // Concatenar las notas originales con las nuevas
-                            totalQuantizedSteps: MELODY.totalQuantizedSteps + sample.totalQuantizedSteps,
+                            ...randomDrumSeed,
+                            notes: [...randomDrumSeed.notes, ...sample.notes],
+                            totalQuantizedSteps: randomDrumSeed.totalQuantizedSteps + sample.totalQuantizedSteps,
                         };
-                        viz = new mm.PianoRollCanvasVisualizer(combinedSequence, document.getElementById('canvas'), config);
+                        setupVisualizer(combinedSequence);
                         rnnPlayer.start(combinedSequence);
                         
                     })
@@ -96,16 +90,28 @@ export default function Page() {
 
 
         };
+        
+        const stopClick = () => {
+            if (rnnPlayer.isPlaying()) {
+                rnnPlayer.stop(); // Detiene la secuencia en reproducción
+            } else {
+                console.log('No hay secuencia en reproducción.');
+            }
+        };
+        
+        
 
-        if (button) {
-            button.addEventListener('click', handleClick);
-        }
+        if (playBtn) 
+            playBtn.addEventListener('click', playClick);
+        if (stopBtn)
+            stopBtn.addEventListener('click', stopClick);
 
         // Limpieza para evitar duplicados
         return () => {
-            if (button) {
-                button.removeEventListener('click', handleClick);
-            }
+            if (playBtn) 
+                playBtn.removeEventListener('click', playClick);
+            if (stopBtn)
+                stopBtn.removeEventListener('click', stopClick);
         };
 
 
@@ -116,7 +122,8 @@ export default function Page() {
         <div className="flex flex-col items-center">   
             <h1>FLOW-FI</h1>
             <canvas id="canvas" className=" bg-black border border-gray-700"></canvas>
-            <button id="button">Play</button>
+            <button id="play">Play</button>
+            <button id="stop">Stop</button>
             <script src="https://cdn.jsdelivr.net/npm/@magenta/music@^1.23.1"></script>
         </div>
     );
---
+++ src/app/seeds.js
@@ -0,0 +1,79 @@
+export const seeds = {
+    drumsSeeds: [
+      { 
+        id: 1,
+        ticksPerQuarter: 220,
+        totalQuantizedSteps: 8,
+        quantizationInfo: { stepsPerQuarter: 4 },
+        notes: [
+          { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1 },
+          { pitch: 42, quantizedStartStep: 1, quantizedEndStep: 2 },
+          { pitch: 36, quantizedStartStep: 4, quantizedEndStep: 5 },
+          { pitch: 42, quantizedStartStep: 5, quantizedEndStep: 6 },
+          { pitch: 42, quantizedStartStep: 7, quantizedEndStep: 8 }
+        ]
+      },
+      { 
+        id: 2,
+        ticksPerQuarter: 220,
+        totalQuantizedSteps: 8,
+        quantizationInfo: { stepsPerQuarter: 4 },
+        notes: [
+          { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1 },
+          { pitch: 42, quantizedStartStep: 0, quantizedEndStep: 1 },
+          { pitch: 42, quantizedStartStep: 2, quantizedEndStep: 3 },
+          { pitch: 36, quantizedStartStep: 4, quantizedEndStep: 5 },
+          { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5 },
+          { pitch: 42, quantizedStartStep: 6, quantizedEndStep: 7 }
+        ]
+      },
+      {
+        id: 3,
+        ticksPerQuarter: 220,
+        totalQuantizedSteps: 8,
+        quantizationInfo: { stepsPerQuarter: 4 },
+        notes: [
+          { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1 }, // Kick
+          { pitch: 42, quantizedStartStep: 0, quantizedEndStep: 1 }, // Hi-hat
+          { pitch: 42, quantizedStartStep: 1, quantizedEndStep: 2 }, // Hi-hat
+          { pitch: 36, quantizedStartStep: 3, quantizedEndStep: 4 }, // Kick
+          { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5 }, // Hi-hat
+          { pitch: 36, quantizedStartStep: 6, quantizedEndStep: 7 }, // Kick
+          { pitch: 42, quantizedStartStep: 6, quantizedEndStep: 7 }  // Hi-hat
+        ]
+      }, 
+      {
+        id: 4,
+        ticksPerQuarter: 220,
+        totalQuantizedSteps: 8,
+        quantizationInfo: { stepsPerQuarter: 4 },
+        notes: [
+          { pitch: 42, quantizedStartStep: 0, quantizedEndStep: 1 }, // Hi-hat
+          { pitch: 42, quantizedStartStep: 2, quantizedEndStep: 3 }, // Hi-hat
+          { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5 }, // Hi-hat
+          { pitch: 42, quantizedStartStep: 6, quantizedEndStep: 7 }  // Hi-hat
+        ]
+      }, 
+      {
+        id: 5,
+        ticksPerQuarter: 220,
+        totalQuantizedSteps: 8,
+        quantizationInfo: { stepsPerQuarter: 4 },
+        notes: [
+          { pitch: 36, quantizedStartStep: 0, quantizedEndStep: 1 }, // Kick
+          { pitch: 42, quantizedStartStep: 1, quantizedEndStep: 2 }, // Hi-hat
+          { pitch: 36, quantizedStartStep: 3, quantizedEndStep: 4 }, // Kick
+          { pitch: 42, quantizedStartStep: 4, quantizedEndStep: 5 }, // Hi-hat
+          { pitch: 42, quantizedStartStep: 6, quantizedEndStep: 7 }  // Hi-hat
+        ]
+      },
+   
+      
+      
+    ],
+  
+    melodySeeds: [ /* Arreglo de patrones para Lookback RNN */ ],
+    bassSeeds: [ /* Arreglo de patrones para Basic Improv */ ]
+  };
+  
+  
