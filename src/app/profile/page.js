155beb093c5ad831ff3b695f00f2c51a3636877c
EddVargs8 <l19290961@tectijuana.edu.mx>
2024-11-22 15:13:10 -0800

    Added logout button on profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,5 +1,14 @@
 // Profile page
+'use client'
+import { signOut } from "next-auth/react";
 
 export default function Page() {
-    return <h1>Hello, Profile page! uwu </h1>
+    return (
+        <button
+          onClick={() => signOut({ callbackUrl: "/" })} // Redirige al home después de logout
+          className="p-2 bg-red-500 text-white rounded hover:bg-red-600"
+        >
+          Cerrar sesión
+        </button>
+      );
 } 
